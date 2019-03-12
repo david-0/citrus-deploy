13fb2390d5493e1849f6b015ba4c5795e87862a5
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-12 21:48:12 +0100

    change deploy script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -8,15 +8,27 @@ cd ${s%/*}
 echo "++++ build client" && \
 	cd ../citrus-client && \
 	npm run build-prod && \
-	scp -r dist $server:~/website/citrus-client/dist && \\
 	ssh $server << EOF 
-	cd ~/website/citrus/citrus-client && \
+	echo "---- clean dist" && \\
+	rm -rf ~/website/citrus/citrus-client/dist
+	echo "---- create dist" && \\
+	mkdir -p ~/website/citrus/citrus-client/dist
+EOF
+[ $? -eq 0 ] && \
+	echo "++++ copy site" && \
+	scp -r dist $server:~/website/citrus/citrus-client && \\
+	ssh $server << EOF 
+	cd ~/website/citrus/citrus-client && \\
+	echo "---- client: git pull" && \\
 	git pull && \\
+	echo "---- client: run deploy" && \\
 	npm run deploy
 EOF
 [ $? -eq 0 ] && \
 	echo "++++ build server" && \
-	ssh $server << EOF 
+	ssh $server << EOF
+	cd ~/website/citrus/citrus-server && \\
+	echo "---- server: run reload" && \\
 	npm run reload
 EOF
 [ $? -eq 0 ] && \
