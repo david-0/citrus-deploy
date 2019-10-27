5076953c22918a4296001265ea3d3962bbb4f29f
David Leuenberger <david.leuenberger@gmx.ch>
2019-10-27 21:06:05 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,34 @@ if [[ "$1" == "-h" ]]; then
 	exit 1;
 fi;
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+exec() {
+	echo -n "$@ ... "
+#	eval $@
+	if [[ $? -eq 0 ]]; then
+		echo -e "${GREEN}ok${NC}";
+	else
+		echo -e "${RED}failed${NC}"
+		exit 1;
+	fi;
+}
+
+# parameter 1: systemdServiceName
+updateDbSettings() {
+	if [[ "$1" = "citrus-int" ]]; then
+		exec sed -i 's/"database": "citrus"/"database": "citrus-int"/g' ormconfig.json
+	fi;
+}
+
 level=partial
 
-if [[ "$pwd" = "/home/alixon/usr/davidl/website/citrus-prod/citrus-deploy" ]] ; then
+if [[ "$(pwd)" = "/home/alixon/usr/davidl/website/citrus-prod/citrus-deploy" ]] ; then
 	systemdServiceName=citrus-prod;
 fi;
-if [[ "$pwd" = "/home/alixon/usr/davidl/website/citrus-int/citrus-deploy" ]] ; then
+if [[ "$(pwd)" = "/home/alixon/usr/davidl/website/citrus-int/citrus-deploy" ]] ; then
 	systemdServiceName=citrus-int;
 fi;
 if [[ "$1" = "--full" ]]; then
@@ -19,31 +41,44 @@ if [[ "$1" = "--full" ]]; then
 fi;
 if [[ "$systemdServiceName" = "" ]]; then
 	echo "no service found";
-#	exit 1;
+#	exit 1
 fi;
-
-echo "cd .."
-echo "rm -rf citrus-server"
-echo "git clone https://github.com/david-0/citrus-server.git"
-echo "cd citrus-server"
-echo "npm install"
-echo "npm run build"
-echo "cd .."
-
+cd ..
 if [[ "$level" = "full" ]] || [[ ! -d "citrus-client" ]] ; then
-	echo "rm -rf citrus-client"
-	echo "git clone https://github.com/david-0/citrus-client.git"
-	echo "cd citrus-client"
-	echo "npm install"
+	exec rm -rf citrus-server
+	exec git clone https://github.com/david-0/citrus-server.git
+	exec cd citrus-server
+	updateDbSettings ${systemdServiceName}
+	exec npm install
+	exec npm run build
+	exec cd ..
+
+	exec rm -rf citrus-client
+	exec git clone https://github.com/david-0/citrus-client.git
+	exec cd citrus-client
+	exec npm install
+	exec npm run build-prod
+	exec npm run deploy
+	exec cd ..
+	
+	exec sudo systemctl stop ${systemdServiceName}
+	exec rm -rf citrus-run
+	exec mv citrus-server citrus-run
+	exec sudo systemctl start ${systemdServiceName}
 else 
-	echo "cd citrus-client"
-	echo "git reset --hard"
-	echo "git clean -fd"
+	exec sudo systemctl stop ${systemdServiceName}
+	exec cd citrus-server
+	exec git reset --hard
+	exec git clean -fd
+	updateDbSettings ${systemdServiceName}
+	exec rm -rf dist
+	exec npm run build
+	exec cd ..
+	
+	exec cd citrus-client
+	exec git reset --hard
+	exec git clean -fd
+	exec npm run build-prod
+	exec npm run deploy
+	exec cd ..
 fi;
-echo "npm run build-prod"
-echo "npm run deploy"
-echo "cd .."
-echo "sudo systemctl stop ${systemdServiceName}"
-echo "rm -rf citrus-run"
-echo "mv citrus-server citrus-run"
-echo "sudo systemctl start ${systemdServiceName}"
