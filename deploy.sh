7e3dcd074927dd8f2bde2af5ac2a9e224bf4e50d
David Leuenberger <david.leuenberger@gmx.ch>
2019-10-28 07:35:50 +0100

    extract to functions to common.sh

--- deploy.sh
+++ deploy.sh
@@ -9,26 +9,7 @@ fi;
 s=$(readlink -f $0)
 cd ${s%/*}
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
-processExitStatus() {
-	if [[ $? -eq 0 ]]; then
-		echo -e " ${GREEN}ok${NC}";
-	else
-		echo -e " ${RED}failed${NC}"
-		exit 1;
-	fi;
-}
-
-exec() {
-	echo -n "${YELLOW}>>>>${NC} $@ ... "
-	eval $@
-	echo -n "${YELLOW}<<<<${NC} $@ "
-	processExitStatus
-}
+. ./common.sh
 
 # parameter 1: systemdServiceName
 updateDbSettings() {
