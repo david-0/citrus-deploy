afc07ff0f476b38910d2199eefa9ee9de3418eb2
David Leuenberger <david.leuenberger@gmx.ch>
2019-10-28 21:59:40 +0100

    use new deploy scripts

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,33 +5,28 @@ server=davidl@88.99.118.38
 s=$(readlink -f $0)
 cd ${s%/*}
 
-echo "++++ build client" && \
-	cd ../citrus-client && \
-	npm run build-prod && \
-	ssh $server << EOF 
-	echo "---- clean dist" && \\
-	rm -rf ~/website/citrus/citrus-client/dist
-	echo "---- create dist" && \\
-	mkdir -p ~/website/citrus/citrus-client/dist
-EOF
-[ $? -eq 0 ] && \
-	echo "++++ copy site" && \
-	scp -r dist $server:~/website/citrus/citrus-client && \
-	ssh $server << EOF 
-	cd ~/website/citrus/citrus-client && \\
-	echo "---- client: git pull" && \\
-	git pull && \\
-	echo "---- client: run deploy" && \\
-	npm run deploy
-EOF
-[ $? -eq 0 ] && \
-	echo "++++ build server" && \
-	ssh $server << EOF
-	cd ~/website/citrus/citrus-server && \\
-	echo "---- server: run reload" && \\
-	npm run reload
-EOF
-[ $? -eq 0 ] && \
-	echo "++++ done"
+. ./common.sh
 
-
+exec ssh $server << EOF
+	~/website/citrus-prod/citrus-deploy/prepare-for-client-upload.sh
+EOF
+if [[ "$1" = "--full" ]] || [[ "$2" = "--full" ]] ; then
+	level="--full"
+	exec rm -rf citrus-client
+	exec git clone https://github.com/david-0/citrus-client.git
+	exec cd citrus-client
+	exec npm install
+	exec npm run build-prod
+	exec cd ..
+else
+	exec cd citrus-client
+	exec git reset --hard
+	exec git clean -fd
+	exec git pull
+	exec npm run build-prod
+	exec cd ..
+fi;
+exec scp -r citrus-client/dist $server:~/website/citrus-prod/prebuilt-client
+exec ssh $server << EOF
+	~/website/citrus-prod/citrus-deploy/deploy.sh $level --use-prebuilt-client
+EOF
